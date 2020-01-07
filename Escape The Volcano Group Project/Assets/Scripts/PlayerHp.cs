6490a4f06e4974fc2c32e6dd1b970af8d602b019
Noe Alonso <20.noe.alonso@ksd.org>
2020-01-07 12:52:20 -0800

    Added scripts

---
+++ Escape The Volcano Group Project/Assets/Scripts/PlayerHp.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class PlayerHp : MonoBehaviour
+{
+    public int health = 10;
+    public Text healthText;
+    public Slider healthSlider;
+    public int lives = 10;
+
+    private void Start()
+    {
+        healthText.text = "Health" + health;
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+        //PlayerPrefs.SetInt("lives", lives);
+        lives = PlayerPrefs.GetInt("lives");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == ("Enemy"))
+        {
+            health--;
+            healthText.text = "Health: " + health;
+            healthSlider.value = health;
+            if (health < 1) 
+            {
+                if(lives > 0)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    //SceneManager.LoadScene("Lose");
+                    PlayerPrefs.SetInt("lives", lives - 1);
+                }
+                else
+                {
+                    SceneManager.LoadScene("GamerOver");
+                }
+                
+
+            }
+        }
+    }
+}
