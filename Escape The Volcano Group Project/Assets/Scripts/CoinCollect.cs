6964dc1bb127f6c9429a29343e1d8fab88418f5c
Noe Alonso <20.noe.alonso@ksd.org>
2020-01-07 13:24:01 -0800

    Scripts

---
+++ Escape The Volcano Group Project/Assets/Scripts/CoinCollect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCollect : MonoBehaviour
+{
+    public int coinCount = 0;
+    public Text coinText;
+
+    private void Start() => coinText.text = "Coin" + coinCount;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.tag == "Coin")
+        {
+            coinCount++;
+            coinText.text = "Coin" + coinCount;
+            Destroy(collision.gameObject);
+        }
+    }
+}
+   
