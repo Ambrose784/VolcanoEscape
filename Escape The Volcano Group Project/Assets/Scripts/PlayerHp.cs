a1953f026d56ca6197558362394bd3c87e078443
Noe Alonso <20.noe.alonso@ksd.org>
2020-01-08 12:23:56 -0800

    Changes to scripts

--- Escape The Volcano Group Project/Assets/Scripts/PlayerHp.cs
+++ Escape The Volcano Group Project/Assets/Scripts/PlayerHp.cs
@@ -22,6 +22,29 @@ public class PlayerHp : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.tag == ("DeathBarrier"))
+        {
+            health--;
+            healthText.text = "Health: " + health;
+            healthSlider.value = health;
+            if (health < 4)
+            {
+                if (health < 4)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                    SceneManager.LoadScene("Lose");
+                    Player
+                }
+              
+            }
+
+        }
+
+        if (collision.gameObject.tag == ("TileDeath"))
+        {
+            Destroy(gameObject);
+        }
+
         if (collision.gameObject.tag == ("Enemy"))
         {
             health--;
