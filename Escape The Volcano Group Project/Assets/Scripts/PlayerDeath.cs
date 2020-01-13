7fdf94a43854a05a9e43ad99ab5bbe7b048016b7
Noe Alonso <20.noe.alonso@ksd.org>
2020-01-13 12:06:26 -0800

    Added testing area for Noe

--- Escape The Volcano Group Project/Assets/Scripts/PlayerDeath.cs
+++ Escape The Volcano Group Project/Assets/Scripts/PlayerDeath.cs
@@ -5,16 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class PlayerDeath: MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "vboi" || collision.gameObject.tag == "spike" || collision.gameObject.tag == "Enemy")
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-        if (collision.gameObject.tag == "portal")
-        {
-            SceneManager.LoadScene("Level 2");
-        }
-    
+         {
+         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+         }
+         if (collision.gameObject.tag == "portal")
+         {
+         SceneManager.LoadScene("Level 2");
+         }
     }
 }
